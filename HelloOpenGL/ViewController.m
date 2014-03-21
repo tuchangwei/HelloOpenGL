5ebc537df6ae180b58521d198684bb4dbfcd1de6
Changwei Tu <changweitu@gmail.com>
2014-03-21 16:31:14 +0800

    Initial Commit

---
+++ HelloOpenGL/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HelloOpenGL
+//
+//  Created by Leelen-mac1 on 14-3-21.
+//  Copyright (c) 2014年 Leelen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
